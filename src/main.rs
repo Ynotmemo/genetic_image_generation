522b396bcde618100cee20319f1e98444ea1b934
Ynotmemo <89087490+Ynotmemo@users.noreply.github.com>
2023-04-14 01:13:45 +0900

    Add fn get_largest_two_fitness and this test

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ use image::{DynamicImage, GenericImageView, ImageBuffer, Rgb};
 use rand::distributions::Uniform;
 use rand::prelude::*;
 use rayon::prelude::*;
+
 mod utils;
 
-const POPULATIONS: usize = 50;
+const POPULATIONS: usize = 10;
 const SEED: u64 = 0;
 const IMAGE_SIZE: (u32, u32) = (400, 400);
 
 // 各個体の構造体を定義
+#[derive(Clone)]
 struct Individual {
     genom_image_buffer: ImageBuffer<Rgb<u8>, Vec<u8>>,
     genom_dynamic_image: DynamicImage,
@@ -62,6 +64,31 @@ fn initialize_generation(populations: usize, image_size: (u32, u32), seed: u64) 
     generation
 }
 
+// 世代ごとに適応度の高い２個体を選択
+fn get_largest_two_fitness(generation: &[Individual]) -> (Individual, Individual) {
+    // 世代の長さを取得
+    let len = generation.len();
+
+    // 世代が空または長さが1の場合はエラーを返す
+    if len == 0 {
+        panic!("No individuals in the generation.");
+    } else if len == 1 {
+        panic!("At least two individuals are required in the generation.");
+    }
+
+    // 適応度の降順でソート
+    let mut sorted_generation = generation.to_vec();
+    sorted_generation.sort_unstable_by(|a, b| b.fitness.partial_cmp(&a.fitness).unwrap());
+
+    // 適応度が最も大きい２つの個体を選択
+    let largest_individual = sorted_generation[0].clone();
+    let second_largest_individual = sorted_generation[1].clone();
+
+    (largest_individual, second_largest_individual)
+}
+
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -104,8 +131,20 @@ mod tests {
                 assert!((0.0 <= individual.fitness) && (individual.fitness <= 1.0));
             });
     }
+
+    #[test]
+    fn choice_large_two_fitness() {
+        let mut generation: Vec<Individual> = initialize_generation(POPULATIONS, IMAGE_SIZE, SEED);
+
+        generation[2].fitness = 0.88;
+        generation[5].fitness = 1.0;
+        generation[7].fitness = 0.91;
+        generation[9].fitness = 0.82;
+
+        let (largest_individual, second_largest_individual) =  get_largest_two_fitness(&generation);
+        assert_eq!(largest_individual.fitness, generation[5].fitness);
+        assert_eq!(second_largest_individual.fitness, generation[7].fitness);
+    }
 }
 
-fn main() {
-    unimplemented!();
-}
+fn main() { unimplemented!(); }
