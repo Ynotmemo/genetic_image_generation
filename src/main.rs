5fe881fb2f129469e7af08bc7f8fb71e2f3629cd
Ynotmemo <89087490+Ynotmemo@users.noreply.github.com>
2023-04-17 01:09:01 +0900

    Add fn generate_next_generation

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use image::{DynamicImage, GenericImageView, ImageBuffer, Rgb};
 use rand::distributions::Uniform;
 use rand::prelude::*;
+use rand::Rng;
 use rayon::prelude::*;
+use std::path::Path;
 
 mod utils;
 
 const POPULATIONS: usize = 10;
 const SEED: u64 = 0;
 const IMAGE_SIZE: (u32, u32) = (400, 400);
+const CROSS_RATE: f64 = 0.5;
 
 // 各個体の構造体を定義
 #[derive(Clone)]
@@ -33,15 +36,7 @@ impl Individual {
     }
 }
 
-// 画像ファイルの読み込み
-fn load_image(file_path: &str) -> DynamicImage {
-    image::open(file_path).expect("Failed to open image")
-}
 
-// 画像サイズの変更
-fn resize_image(image: DynamicImage, width: u32, height: u32) -> DynamicImage {
-    image.resize_exact(width, height, image::imageops::FilterType::Lanczos3)
-}
 
 // 第一世代を生成
 fn initialize_generation(populations: usize, image_size: (u32, u32), seed: u64) -> Vec<Individual> {
@@ -87,7 +82,42 @@ fn get_largest_two_fitness(generation: &[Individual]) -> (Individual, Individual
     (largest_individual, second_largest_individual)
 }
 
+// 2個体を交叉させて新しい個体を生成する関数
+fn crossover_individuals(individual1: &Individual, individual2: &Individual, image_size: (u32, u32), cross_rate: f64) -> Individual {
 
+    let (width, height) = image_size;
+    let pixels: Vec<_> = (0..height)
+        .into_par_iter()
+        .flat_map(|h| {
+            (0..width)
+                .into_par_iter()
+                .map(move |w| {
+                    let mut rng = thread_rng();
+                    let choice = rng.gen_bool(cross_rate);
+                    let pixel = if choice {
+                        *individual1.genom_image_buffer.get_pixel(w, h)
+                    } else {
+                        *individual2.genom_image_buffer.get_pixel(w, h)
+                    };
+                    pixel
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect();
+
+    let pixels_u8: Vec<u8> = pixels.into_iter().map(|p| p.0).flatten().collect();
+    let genom_image_buffer = ImageBuffer::from_vec(width, height, pixels_u8).unwrap();
+    Individual::new(genom_image_buffer)
+}
+
+// 次の世代を生成
+fn generate_next_generation(individual1: &Individual, individual2: &Individual, image_size: (u32, u32), cross_rate: f64, populations: usize, ) -> Vec<Individual> {
+    let crossed_images: Vec<Individual> = (0..populations)
+        .into_par_iter()
+        .map(|_| crossover_individuals(individual1, individual2, image_size, cross_rate))
+        .collect();
+    crossed_images
+}
 
 #[cfg(test)]
 mod tests {
@@ -105,15 +135,15 @@ mod tests {
     #[test]
     fn load_resize_image() {
         let file_path = "./data/target_image.jpeg";
-        let target_image = load_image(file_path);
-        let resized_target_image = resize_image(target_image, IMAGE_SIZE.0, IMAGE_SIZE.1);
+        let target_image = utils::processing_image::load_image(file_path);
+        let resized_target_image = utils::processing_image::resize_image(target_image, IMAGE_SIZE.0, IMAGE_SIZE.1);
         assert_eq!(resized_target_image.dimensions(), IMAGE_SIZE);
     }
 
     #[test]
     fn eva_calculate_ssim() {
         let file_path = "./data/target_image.jpeg";
-        let target_image = load_image(file_path);
+        let target_image = utils::processing_image::load_image(file_path);
         let ssim = utils::ssim::calculate_ssim(&target_image, &target_image);
         assert_eq!(ssim, 1 as f64)
     }
@@ -121,8 +151,8 @@ mod tests {
     #[test]
     fn eva_calc_fitness() {
         let file_path = "./data/target_image.jpeg";
-        let target_image = load_image(file_path);
-        let resized_target_image = resize_image(target_image, IMAGE_SIZE.0, IMAGE_SIZE.1);
+        let target_image = utils::processing_image::load_image(file_path);
+        let resized_target_image = utils::processing_image::resize_image(target_image, IMAGE_SIZE.0, IMAGE_SIZE.1);
         let mut generation: Vec<Individual> = initialize_generation(POPULATIONS, IMAGE_SIZE, SEED);
 
         generation.par_iter_mut()
@@ -147,4 +177,24 @@ mod tests {
     }
 }
 
-fn main() { unimplemented!(); }
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+
+    // unimplemented!();
+    let dad_image = utils::processing_image::load_image("./data/target_image.jpeg");
+    let mom_image = utils::processing_image::load_image("./data/temp_image.jpg");
+    let resized_dad = utils::processing_image::resize_image(dad_image, IMAGE_SIZE.0, IMAGE_SIZE.1);
+    let resized_mom = utils::processing_image::resize_image(mom_image, IMAGE_SIZE.0, IMAGE_SIZE.1);
+    let dad = Individual::new(utils::processing_image::dynamic_image_to_image_buffer(&resized_dad));
+    let mom = Individual::new(utils::processing_image::dynamic_image_to_image_buffer(&resized_mom));
+    let mut child = crossover_individuals(&dad, &mom, IMAGE_SIZE, CROSS_RATE);
+    child.calc_fitness(&resized_dad);
+    println!("similarity to dad: {}", child.fitness);
+    child.calc_fitness(&resized_mom);
+    println!("similarity to mom: {}", child.fitness);
+
+    utils::processing_image::save_dynamic_image_to_png(&child.genom_dynamic_image, "./results/child.png")?;
+
+    Ok(())
+}
